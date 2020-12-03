4e796e8ea65c696b51bf3cc01636b30ebca27e09
Dang Thuyen Vuong <dangthuyenvuong@gmail.com>
2020-12-03 21:00:07 +0700

    update database

--- config/database.js
+++ config/database.js
@@ -2,7 +2,13 @@ module.exports = {
     default: 'mongodb',
     mongodb: {
         // stringConnect: `mongodb+srv://pnj:FkYkKnKPRnop79Xt@cluster0.ticyv.mongodb.net`  // connect throught internet
-        stringConnect: "mongodb://localhost:27017",
-        database: 'nodejs-mvc'
+        // stringConnect: "mongodb://localhost:27017",
+        // database: 'nodejs-mvc'
+
+
+        database: 'nodejs-mvc',
+        USERNAME: 'pnj',
+        PASSWORD: 'FkYkKnKPRnop79Xt',
+        stringConnect: `mongodb+srv://pnj:FkYkKnKPRnop79Xt@cluster0.ticyv.mongodb.net`
     }
 }
