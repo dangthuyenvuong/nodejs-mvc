f9a6521680eb44f68e916d625b3ad5a5e0c3e138
Dang Thuyen Vuong <dangthuyenvuong@gmail.com>
2020-12-14 00:36:42 +0700

    update

--- plugins/backend/index.js
+++ plugins/backend/index.js
@@ -1,6 +1,6 @@
 const path = require('path');
-global.backend_view =  path.join(__dirname, 'views');
+global.backend_view = path.join(__dirname, 'views');
 
-module.exports = function(app){
+module.exports = function (app) {
     app.use('/' + process.env.ADMIN_URL, require('./routers/page'));
 }
